eac6c13d7458e39841704ccfe7891c68251afe96
Swateek Jena <swateek@everestnetworks.com>
2020-04-05 18:57:19 +0530

    Adding Google Analytics Tracking, improving icons

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,8 +1,19 @@
 'use strict';
 
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-162779427-1']);
+
 chrome.browserAction.onClicked.addListener(function(activeTab){
+
   chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-	    let url = tabs[0].url;
+      _gaq.push(['_trackPageview']);
+      let url = tabs[0].url;
 	    chrome.windows.create({"url": url, "incognito": true});
   });
 });
