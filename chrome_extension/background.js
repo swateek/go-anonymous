1840399db1438a0231b141a93bd767cd820224a6
Swateek Jena <swateek08@gmail.com>
2025-11-26 12:56:16 +0530

    #1 - updating on latest changes

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,8 +1,6 @@
 'use strict';
 
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-      let url = tabs[0].url;
-	    chrome.windows.create({"url": url, "incognito": true});
-  });
+chrome.action.onClicked.addListener(function (activeTab) {
+  let url = activeTab.url;
+  chrome.windows.create({ "url": url, "incognito": true });
 });
